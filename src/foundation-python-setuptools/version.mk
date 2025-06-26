12154a7eacfa232933c85e3642ae65fc52e5d31d
Juan Eduardo Ramirez <juaneduardo.ramirez@upr.edu>
2025-06-26 16:41:48 -0400

    updated python-foundation-setuptools version and Makefile for el9

--- src/foundation-python-setuptools/version.mk
+++ src/foundation-python-setuptools/version.mk
@@ -1,5 +1,14 @@
-PYPKGNAME = setuptools
+PYPKGNAME = distribute
 NAME	= foundation-python-setuptools
-VERSION = 39.0.1
+VERSION = 0.6.10
 RELEASE = 0
 RPM.FILES = "/opt/rocks/bin/*\\n/opt/rocks/lib/python2*/site-packages/*"
+ifeq ($(strip $(VERSION.MAJOR)), 8)
+PYPKGNAME = setuptools
+VERSION = 39.0.1
+endif
+ifeq ($(strip $(VERSION.MAJOR)), 9)
+PYPKGNAME = setuptools
+VERSION = 53.0.0
+RPM.FILES = "/opt/rocks/lib/python3*/site-packages/*"
+endif
